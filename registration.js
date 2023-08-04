c4972cac30ca764e0e01ce561692ddddf8d64b06
VanessaKasei <kaseivanessa@gmail.com>
2023-08-04 23:26:23 +0300

    creating user with email and password

--- registration.js
+++ registration.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBiYBkVLrObNIaVcaRKevmgdPteZipxFkM",
   authDomain: "expense-tracker-fef7a.firebaseapp.com",
@@ -8,11 +12,77 @@ const firebaseConfig = {
   appId: "1:1032418652697:web:e3c06c01150d99d81fb088",
   measurementId: "G-D7CLLGRM6V"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+document.getElementById("upButton").addEventListener("click", registerUser); 
+    // Call the registerUser() function when the button is clicked
+
+// Function to handle user registration
+function registerUser() {
+  // Get form field values
+  const email = document.getElementById("emailid").value;
+  const password = document.getElementById("password").value;
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+     // Registration successful, access the user data
+    const user = userCredential.user;
+    console.log("User registered:", user.email);
+
+      // Redirect the user to page1.html after successful registration
+    window.location.href = "login.html";
+
+      // Display a success message to the user
+    const successMessage = document.getElementById("success-message");
+    successMessage.style.display = "block";
+  })
+  .catch((error) => {
+    
+    //const errorCode = error.code;
+      // Handle registration errors
+    const errorMessage = error.message;
+          console.error("Registration error:", errorMessage);
+
+      // Display an error message to the user
+      const errorMessageElement = document.getElementById("error-message");
+      errorMessageElement.style.display = "block";
+      errorMessageElement.innerText = errorMessage;
+
+  });
+}
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //initialize firebase 
 firebase.initializeApp(firebaseConfig);//initializes firebase app with the provided configuration
 
 //reference your firebase
-var registrationForm = firebase.database().ref("registrationForm");
+var registrationFormDB = firebase.database().ref("registrationForm");
 
 document.getElementById("regForm").addEventListener("submit", submitForm);
 
@@ -31,22 +101,22 @@ function submitForm(e){
     //remove alert
     setTimeout(() => { 
     document.querySelector(".alert").style.display = "block";
-    },2000);
+    },3000);
 
     // reset the form
     document.getElementById("regForm").reset();
 }
 
 const saveDetails = (name, userName, emailid) => {
-  var newRegistrationForm = registrationForm.push();
+  var newRegistrationForm = registrationFormDB.push();
 
   newRegistrationForm.set({
     name : name,
     userName : userName,
     emailid : emailid,
   });
-}
+};
 
 const getElementVal = (id) => {
     return document.getElementById(id).value;
-}
+};*/
